04f20257f29ddb006de8a11c379f1cb0c8b43d36
shaineblack <shaineblck@gmail.com>
2024-01-22 15:31:48 -0500

    events lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left + 1 < 360) { // Assuming 360 is the right boundary
+      dodger.style.left = `${left + 1}px`;
+    }
+}
